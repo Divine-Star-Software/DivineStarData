4fbbdc131ea31bbab53f69f4171b91453d32c3dc
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2021-11-27 09:50:24 -0500

    updated readme and added onError option to read and write

--- ts/index.ts
+++ ts/index.ts
@@ -3,7 +3,7 @@ import { LZMACompress } from "./LZMACompress.js";
 import { LMZADecompress } from "./LMZADecompress.js";
 /**# Divine Star Data
  * ---
- * Handles the reading and writing of compressed JSON. 
+ * Handles the reading and writing of compressed JSON.
  */
 export class DivineStarData {
   defaultLevel = 9;
@@ -14,22 +14,23 @@ export class DivineStarData {
   /**# Set Compression Level
    * ---
    * Default is 9. Lowest is 1;
-   * @param level 
+   * @param level
    */
-  setCompressionLevel(level : number) {
-    if(level > 9 || level < 1) {
-      throw new Error()
+  setCompressionLevel(level: number) {
+    if (level > 9 || level < 1) {
+      throw new Error();
     }
     this.defaultLevel = level;
   }
   /**# Read
    * ---
-   * Read in a compressed JSON file and return it as a JSON object. 
-   * @param path 
-   * @returns 
+   * Read in a compressed JSON file and return it as a JSON object.
+   * @param path
+   * @param onError - optional - Function to run if error occurs. Will be passed the error obj.
+   * @returns data | null if error
    */
-  async read(path: string) : Promise<any> {
-    let data : any = null;
+  async read(path: string, onError?: Function): Promise<any> {
+    let data: any = null;
     try {
       const rawBuffer = await this.fileSystem.readFile(path);
       const deCompressed = this.decompressor.decompress(rawBuffer);
@@ -37,16 +38,20 @@ export class DivineStarData {
     } catch (error: any) {
       console.log(`Problem loading: ${path}`);
       console.log(error);
+      if (onError) {
+        onError(error);
+      }
     }
     return data;
   }
-/**# Write
- * ---
- * Write a JSON object to the given path in a compressed format. 
- * @param path 
- * @param data 
- */
-  async write(path: string, data: object) {
+  /**# Write
+   * ---
+   * Write a JSON object to the given path in a compressed format.
+   * @param path
+   * @param data
+   * @param onError - optional - Function to run if error occurs. Will be passed the error obj.
+   */
+  async write(path: string, data: object, onError?: Function) {
     try {
       const rawJson = JSON.stringify(data);
       const compressed = this.compressor.compress(rawJson, this.defaultLevel);
@@ -55,6 +60,9 @@ export class DivineStarData {
     } catch (error: any) {
       console.log(`Problem writing: ${path}`);
       console.log(error);
+      if (onError) {
+        onError(error);
+      }
     }
   }
 }
